2aab63cbcef24832c492b496549a9757e482e74d
안강민 <hkkm612@gmail.com>
2024-02-27 16:06:18 +0900

    feat: [bool] Boolean 관련 패키지 생성

---
+++ packages/common/bool_dogoo/example/bool_dogoo_example.dart
@@ -0,0 +1,6 @@
+import 'package:bool_dogoo/bool_dogoo.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/common/bool_dogoo/test/bool_dogoo_test.dart
@@ -0,0 +1,16 @@
+import 'package:bool_dogoo/bool_dogoo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
