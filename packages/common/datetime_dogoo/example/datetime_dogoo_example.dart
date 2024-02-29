85d4242d4efb49987d57198a38eea62bb5e31b86
안강민 <hkkm612@gmail.com>
2024-02-29 16:19:41 +0900

    feat : [DateTime] 예제 코드 추가

--- packages/common/datetime_dogoo/example/datetime_dogoo_example.dart
+++ packages/common/datetime_dogoo/example/datetime_dogoo_example.dart
@@ -1,3 +1,17 @@
 import 'package:datetime_dogoo/datetime_dogoo.dart';
 
-void main() {}
+void main() {
+  final today = DateTime.now();
+
+  print(today.toStringAsFormated(format: DateTimeStringFormat.YYYYMMDD)); // 2024/02/29
+  print(today.toStringAsFormated(format: DateTimeStringFormat.YYYYMMDD_hhmmss)); // 2024/02/29 15:53:18
+  print(today.toStringAsFormated(format: DateTimeStringFormat.MMDDYYYY)); // 02/29/2024
+
+  print(today.toStringAsFormated(format: DateTimeStringFormat.YYYYMMDD, dateSeparator: '-', timeSeparator: '::')); // 2024-02-29
+  print(today.toStringAsFormated(format: DateTimeStringFormat.YYYYMMDD_hhmmss, dateSeparator: '-', timeSeparator: '::')); // 2024-02-29 15::53::18
+  print(today.toStringAsFormated(format: DateTimeStringFormat.MMDDYYYY, dateSeparator: '-', timeSeparator: '::')); // 02-29-2024
+
+  print(today.toStringAsFormated(format: DateTimeStringFormat.YYYYMMDD, dateSeparator: '.')); // 2024.02.29
+  print(today.toStringAsFormated(format: DateTimeStringFormat.YYYYMMDD_hhmmss, dateSeparator: '.')); // 2024.02.29 15:53:18
+  print(today.toStringAsFormated(format: DateTimeStringFormat.MMDDYYYY, dateSeparator: '.')); // 02.29.2024
+}
