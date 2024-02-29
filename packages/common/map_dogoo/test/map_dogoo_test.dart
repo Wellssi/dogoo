0136a84444a9ac74150060963095b448da7598ed
안강민 <hkkm612@gmail.com>
2024-02-29 16:38:28 +0900

    test : [Map] MapDogoo 비어있는 테스트 코드 추가.

--- packages/common/map_dogoo/test/map_dogoo_test.dart
+++ packages/common/map_dogoo/test/map_dogoo_test.dart
@@ -2,7 +2,25 @@ import 'package:map_dogoo/map_dogoo.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    test('First Test', () {});
+  group('📌 MapDogoo/Map - Function Test', () {
+    test('👉 전달하는 값이 null이 아니면 Map에 추가하는 함수', () {});
+    test('👉 전달하는 키에 대한 값이 null이 아니면 반환하는 함수', () {});
+  });
+
+  group('📌 MapDogoo/Map - Extension Test', () {
+    test('👉 전달하는 값이 null이 아니면 Map에 추가하는 함수', () {});
+    test('👉 전달하는 키에 대한 값이 null이 아니면 반환하는 함수', () {});
+  });
+
+  group('📌 MapDogoo/Json - Function Test', () {
+    test('👉 전달하는 값이 null이 아니면 Json에 추가하는 함수', () {});
+    test('👉 전달하는 키에 대한 값이 null이 아니면 반환하는 함수', () {});
+    test('👉 JSON을 보기 편한 문자열로 전달하는 함수', () {});
+  });
+
+  group('📌 MapDogoo/Json - Extension Test', () {
+    test('👉 전달하는 값이 null이 아니면 Json에 추가하는 함수', () {});
+    test('👉 전달하는 키에 대한 값이 null이 아니면 반환하는 함수', () {});
+    test('👉 JSON을 보기 편한 문자열로 전달하는 함수', () {});
   });
 }
