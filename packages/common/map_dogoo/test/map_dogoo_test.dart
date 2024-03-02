b9c7f06424096a75b37c74f85e210fe4fd51fc0c
안강민 <hkkm612@gmail.com>
2024-03-02 14:10:05 +0900

    test : [Map] Extension 테스트 코드 작성

--- packages/common/map_dogoo/test/map_dogoo_test.dart
+++ packages/common/map_dogoo/test/map_dogoo_test.dart
@@ -31,8 +31,31 @@ void main() {
   });
 
   group('📌 MapDogoo/Map - Extension Test', () {
-    test('👉 전달하는 값이 null이 아니면 Map에 추가한다', () {});
-    test('👉 전달하는 키에 대한 값이 null이 아니면 반환한다', () {});
+    test('👉 전달하는 값이 null이 아니면 Map에 추가한다', () {
+      Map<String, dynamic> myMap = {
+        'key1': 1,
+      };
+      myMap.setIfPresent('key2');
+
+      expect(myMap, {'key1': 1});
+
+      myMap.setIfPresent('key2', value: 2);
+
+      expect(myMap, {'key1': 1, 'key2': 2});
+
+      myMap.setIfPresent('key3', value: null, defaultValue: 3);
+
+      expect(myMap, {'key1': 1, 'key2': 2, 'key3': 3});
+    });
+
+    test('👉 전달하는 키에 대한 값이 null이 아니면 반환한다', () {
+      Map<String, dynamic> myMap = {
+        'key1': 1,
+      };
+      expect(myMap.getIfPresent('key1'), 1);
+      expect(myMap.getIfPresent('key2'), null);
+      expect(myMap.getIfPresent('key2', defaultValue: 2), 2);
+    });
   });
 
   group('📌 MapDogoo/Json - Function Test', () {
