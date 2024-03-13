878d29e6bcf096fe1d8fd30529965bd53ae20548
안강민 <hkkm612@gmail.com>
2024-03-13 15:00:14 +0900

    feat(string): function에 대한 example 추가

--- packages/common/string_dogoo/example/string_dogoo_example.dart
+++ packages/common/string_dogoo/example/string_dogoo_example.dart
@@ -1,21 +1,31 @@
 import 'package:string_dogoo/string_dogoo.dart';
 
 void main() {
+  print("${stringToBool("true")}, ${stringToBool("true").runtimeType}"); // true, bool
+  print("${stringToBool("True")}, ${stringToBool("True").runtimeType}"); // true, bool
+  print("${stringToBool("truE")}, ${stringToBool("truE").runtimeType}"); // true, bool
+
+  print("${stringToBool("false")}, ${stringToBool("false").runtimeType}"); // false, bool
+  print("${stringToBool("False")}, ${stringToBool("False").runtimeType}"); // false, bool
+  print("${stringToBool("falsE")}, ${stringToBool("falsE").runtimeType}"); // false, bool
+
+  print(stringToBool("abcdef")); // null
+  print(stringToBool("")); // null
+
+  print(stringToTitleCase("apple")); // Apple
+  print(stringToTitleCase("banana")); // Banana
+
+  // * -------------------- *
+
   print("${"true".toBool()}, ${"true".toBool().runtimeType}"); // true, bool
   print("${"True".toBool()}, ${"True".toBool().runtimeType}"); // true, bool
-  print("${"tRue".toBool()}, ${"tRue".toBool().runtimeType}"); // true, bool
-  print("${"trUe".toBool()}, ${"trUe".toBool().runtimeType}"); // true, bool
   print("${"truE".toBool()}, ${"truE".toBool().runtimeType}"); // true, bool
 
   print("${"false".toBool()}, ${"false".toBool().runtimeType}"); // false, bool
   print("${"False".toBool()}, ${"False".toBool().runtimeType}"); // false, bool
-  print("${"fAlse".toBool()}, ${"fAlse".toBool().runtimeType}"); // false, bool
-  print("${"faLse".toBool()}, ${"faLse".toBool().runtimeType}"); // false, bool
-  print("${"falSe".toBool()}, ${"falSe".toBool().runtimeType}"); // false, bool
   print("${"falsE".toBool()}, ${"falsE".toBool().runtimeType}"); // false, bool
 
-  print("a".toBool()); // null
-  print("b".toBool()); // null
+  print("abcdef".toBool()); // null
   print("".toBool()); // null
 
   print("apple".toTitleCase()); // Apple
