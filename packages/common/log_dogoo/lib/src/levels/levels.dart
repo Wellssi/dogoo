d00ed46448153c199bc179b19870c182b78c1a34
안강민 <hkkm612@gmail.com>
2024-03-20 14:05:35 +0900

    feat(log): LogLevel 변경사항 적용에 대한 레벨 상수들 변경

--- packages/common/log_dogoo/lib/src/levels/levels.dart
+++ packages/common/log_dogoo/lib/src/levels/levels.dart
@@ -5,47 +5,54 @@ LogLevel traceLevel = LogLevel(
   name: 'TRACE',
   value: 1000,
   symbol: EmojiColor.whiteCircle.value,
-  color: AnsiFontColor.white,
+  fontColor: AnsiFontColor.white,
+  backgroundColor: AnsiBackgroundColor.none,
 );
 
 LogLevel debugLevel = LogLevel(
   name: 'DEBUG',
   value: 2000,
   symbol: EmojiColor.blueCircle.value,
-  color: AnsiFontColor.blue,
+  fontColor: AnsiFontColor.blue,
+  backgroundColor: AnsiBackgroundColor.none,
 );
 
 LogLevel infoLevel = LogLevel(
   name: 'INFO',
   value: 3000,
   symbol: EmojiColor.greenCircle.value,
-  color: AnsiFontColor.green,
+  fontColor: AnsiFontColor.green,
+  backgroundColor: AnsiBackgroundColor.none,
 );
 
 LogLevel warningLevel = LogLevel(
   name: 'WARNING',
   value: 4000,
   symbol: EmojiColor.yellowCircle.value,
-  color: AnsiFontColor.yellow,
+  fontColor: AnsiFontColor.yellow,
+  backgroundColor: AnsiBackgroundColor.none,
 );
 
 LogLevel errorLevel = LogLevel(
   name: 'ERROR',
   value: 5000,
   symbol: EmojiColor.redCircle.value,
-  color: AnsiFontColor.red,
+  fontColor: AnsiFontColor.red,
+  backgroundColor: AnsiBackgroundColor.none,
 );
 
 LogLevel fatalLevel = LogLevel(
   name: 'FATAL',
   value: 6000,
   symbol: EmojiColor.magentaCircle.value,
-  color: AnsiFontColor.magenta,
+  fontColor: AnsiFontColor.magenta,
+  backgroundColor: AnsiBackgroundColor.none,
 );
 
 LogLevel noneLevel = LogLevel(
   name: 'NONE',
   value: 9999,
   symbol: EmojiColor.none.value,
-  color: AnsiFontColor.none,
+  fontColor: AnsiFontColor.none,
+  backgroundColor: AnsiBackgroundColor.none,
 );
