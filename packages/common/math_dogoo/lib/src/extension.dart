d0bf1ece4d0d5be600242396f46e99ef2413f3f1
안강민 <hkkm612@gmail.com>
2024-03-28 10:53:12 +0900

    feat(math): extension에 함수 추가
    - hyperbolic 함수 추가
    - arc hyperbolic 함수 추가
    - factorial 함수 반환 타입 변경
    - log 함수 이름 변경: log -> logN

--- packages/common/math_dogoo/lib/src/extension.dart
+++ packages/common/math_dogoo/lib/src/extension.dart
@@ -7,34 +7,40 @@ extension MathDogooExtension on num {
     return math_dogoo.absolute(this);
   }
 
+  /// Extension of [math_dogoo.acosh].
+  /// Converts to a [double] and returns hyperbolic arccosine value.
+  double acosh() {
+    return math_dogoo.acosh(this);
+  }
+
+  /// Extension of [math_dogoo.asinh].
+  /// Converts to a [double] and returns hyperbolic arcsine value.
+  double asinh() {
+    return math_dogoo.asinh(this);
+  }
+
+  /// Extension of [math_dogoo.atanh].
+  /// Converts to a [double] and returns hyperbolic arctangent value.
+  double atanh() {
+    return math_dogoo.atanh(this);
+  }
+
+  /// Extension of [math_dogoo.cosh].
+  /// Converts to a [double] and returns hyperbolic cosine value.
+  double cosh() {
+    return math_dogoo.cosh(this);
+  }
+
+  /// Extension of [math_dogoo.ee].
+  /// Converts to a [double] and returns x * 10^[y] value.
+  double ee(int y) {
+    return math_dogoo.ee(this, y);
+  }
+
   /// Extension of [math_dogoo.factorial].
   /// Converts to an [int] and returns factorial value.
   double factorial() {
     return math_dogoo.factorial(toInt());
-  }
-
-  /// Extension of [math_dogoo.roundAt].
-  /// Converts to a [double] and returns rounded value.
-  double roundAt({int? fractionDigits}) {
-    return math_dogoo.roundAt(this, fractionDigits: fractionDigits);
-  }
-
-  /// Extension of [math_dogoo.hyperbolicCosine(x)].
-  /// Converts to a [double] and returns hyperbolic cosine value.
-  double hyperbolicCosine() {
-    return math_dogoo.cosh(this);
-  }
-
-  /// Extension of [math_dogoo.hyperbolicSine(x)].
-  /// Converts to a [double] and returns hyperbolic sine value.
-  double hyperbolicSine() {
-    return math_dogoo.sinh(this);
-  }
-
-  /// Extension of [math_dogoo.hyperbolicTangent(x)].
-  /// Converts to a [double] and returns hyperbolic tangent value.
-  double hyperbolicTangent() {
-    return math_dogoo.tanh(this);
   }
 
   /// Extension of [math_dogoo.log2].
@@ -51,7 +57,7 @@ extension MathDogooExtension on num {
 
   /// Extension of [math_dogoo.log10].
   /// Converts to a [double] and returns [base]-base logarithm value.
-  double log(num base) {
+  double logN(num base) {
     return math_dogoo.logN(this, base);
   }
 
@@ -59,5 +65,23 @@ extension MathDogooExtension on num {
   /// Converts to a [double] and returns [n] square root of value.
   double root(num n) {
     return math_dogoo.root(this, n);
+  }
+
+  /// Extension of [math_dogoo.roundAt].
+  /// Converts to a [double] and returns rounded value.
+  double roundAt({int? fractionDigits}) {
+    return math_dogoo.roundAt(this, fractionDigits: fractionDigits);
+  }
+
+  /// Extension of [math_dogoo.sinh].
+  /// Converts to a [double] and returns hyperbolic sine value.
+  double sinh() {
+    return math_dogoo.sinh(this);
+  }
+
+  /// Extension of [math_dogoo.tanh].
+  /// Converts to a [double] and returns hyperbolic tangent value.
+  double tanh() {
+    return math_dogoo.tanh(this);
   }
 }
