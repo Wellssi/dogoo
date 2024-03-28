04a62b61a77931bd89738f51344a67e111988bb2
Kangmin An <hkkm612@gmail.com>
2024-03-28 11:01:51 +0900

    Revert "release(math): v0.2.0"

--- packages/common/math_dogoo/lib/src/functions/ee.dart
+++
@@ -1,5 +0,0 @@
-import 'dart:math' as math;
-
-double ee(num x, num y) {
-  return (x * math.pow(10, y)).toDouble();
-}
--- packages/common/math_dogoo/lib/src/functions/log_2.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:math' as math;
-
-/// Returns the 2 base logarithm of the [x] to a [double].
-double log2(num x) {
-  return math.log(x) / math.ln2;
-}
