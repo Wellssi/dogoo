79fc39df504c94eff32ee657d15d12fd7f0480a9
Kangmin An <hkkm612@gmail.com>
2024-07-16 23:00:59 +0900

    feat(notion): add ChildPageBlock Object

---
+++ packages/common/notion_dogoo/lib/src/objects/blocks/child_page_block.dart
@@ -0,0 +1,48 @@
+part of 'block.dart';
+
+const String _childPage = 'child_database';
+
+/// REF: https://developers.notion.com/reference/block#child-page
+class ChildPageBlock extends Block {
+  ChildPageBlock({
+    required super.id,
+    required super.parent,
+    required super.createdTime,
+    required super.lastEditedTime,
+    required super.creattedBy,
+    required super.lastEditedBy,
+    required super.hasChildren,
+    required super.archived,
+    required super.inTrash,
+    required this.childPageTitle,
+  }) : super(
+          type: BlockType.childPage,
+        );
+
+  final String childPageTitle;
+
+  factory ChildPageBlock.fromJson(Map<String, dynamic> json) {
+    return ChildPageBlock(
+      id: json[_id],
+      parent: Parent.fromJson(json[_parent]),
+      createdTime: DateTime.parse(json[_createdTime]),
+      lastEditedTime: DateTime.parse(json[_lastEditedTime]),
+      creattedBy: User.fromJson(json[_createdBy]),
+      lastEditedBy: User.fromJson(json[_lastEditedBy]),
+      hasChildren: json[_hasChildren],
+      archived: json[_archived],
+      inTrash: json[_inTrash],
+      childPageTitle: json[_childPage][_title],
+    );
+  }
+
+  @override
+  Map<String, Object> toJson() {
+    return {
+      ...super.toJson(),
+      _childPage: {
+        _title: childPageTitle,
+      },
+    };
+  }
+}
