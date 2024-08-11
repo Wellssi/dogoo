effa8c3724fc6e6f6ca06c9a0edf426404e377ab
Kangmin An <hkkm612@gmail.com>
2024-08-11 20:32:16 +0900

    feat(log): add Example

--- packages/common/log_dogoo/example/log_dogoo_example.dart
+++ packages/common/log_dogoo/example/log_dogoo_example.dart
@@ -1,3 +1,10 @@
 import 'package:log_dogoo/log_dogoo.dart';
 
-void main() {}
+Future<void> main() async {
+  await Logger().t('trace message');
+  await Logger().d('debug message');
+  await Logger().i('info message');
+  await Logger().w('warning message');
+  await Logger().e('error message');
+  await Logger().f('fatal message');
+}
