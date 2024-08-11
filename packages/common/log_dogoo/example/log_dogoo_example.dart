4e8ed324b953d6edc381dd54c63025b2c83437d2
Kangmin An <hkkm612@gmail.com>
2024-08-11 20:52:54 +0900

    feat(log): add example code

--- packages/common/log_dogoo/example/log_dogoo_example.dart
+++ packages/common/log_dogoo/example/log_dogoo_example.dart
@@ -7,4 +7,15 @@ Future<void> main() async {
   await Logger().w('warning message');
   await Logger().e('error message');
   await Logger().f('fatal message');
+
+  await Logger()('', level: noneLevel);
+
+  await Logger()(
+    'just message without level will be printed as trace level log',
+  );
+  await Logger()('debug message2', level: debugLevel);
+  await Logger()('info message2', level: infoLevel);
+  await Logger()('warning message2', level: warningLevel);
+  await Logger()('error message2', level: errorLevel);
+  await Logger()('fatal message2', level: fatalLevel);
 }
