b22354f5c951077c0c3cbf8d8f5c0c7c08d9cfd5
Kangmin An <hkkm612@gmail.com>
2024-08-11 15:00:23 +0900

    chore(log): add comments

--- packages/common/log_dogoo/lib/src/core/log_formatter.dart
+++ packages/common/log_dogoo/lib/src/core/log_formatter.dart
@@ -4,6 +4,6 @@ import 'log_print_data.dart';
 abstract class LogFormatter {
   Future<void> init();
 
-  /// [List]<[String]> 으로 반환 혹은 [LogPrintData]로 반환
+  // TODO(me): [List]<[String]> 으로 반환 혹은 [LogPrintData]로 반환
   LogPrintData call(LogData data);
 }
--- packages/common/log_dogoo/lib/src/core/log_result.dart
+++ packages/common/log_dogoo/lib/src/core/log_result.dart
@@ -5,6 +5,8 @@ import 'log_data.dart';
 /// This class is used to return the result of the log.
 class LogResult {
   final bool success;
+
+  /// If the log is filtered, it is set to true.
   final bool filtered;
   final LogData logData;
   final List<String> formattedMessages;
