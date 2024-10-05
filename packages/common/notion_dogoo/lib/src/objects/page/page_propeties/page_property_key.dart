317d2f52c3c5850babfedc5f2427eecb59c537d2
Kangmin An <hkkm612@gmail.com>
2024-10-06 00:00:56 +0900

    feat(notion): add PagePropertyDate

--- packages/common/notion_dogoo/lib/src/objects/page/page_propeties/page_property_key.dart
+++ packages/common/notion_dogoo/lib/src/objects/page/page_propeties/page_property_key.dart
@@ -5,3 +5,5 @@ const String _type = 'type';
 const String _checkbox = 'checkbox';
 const String _createdBy = 'created_by';
 const String _createdTime = 'created_time';
+const String _start = 'start';
+const String _end = 'end';
