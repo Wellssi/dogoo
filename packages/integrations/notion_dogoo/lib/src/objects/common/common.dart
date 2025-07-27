4b150ae2c050356496b69dae3544d39499ed7ca8
Kangmin An <hkkm612@gmail.com>
2025-07-27 22:52:09 +0900

    refactor: migrate RichText from block -> common

--- packages/integrations/notion_dogoo/lib/src/objects/common/common.dart
+++ packages/integrations/notion_dogoo/lib/src/objects/common/common.dart
@@ -1,3 +1,4 @@
 export 'file.dart';
 export 'notion_object.dart';
+export 'rich_text/rich_text.dart';
 export 'user.dart';
--- packages/integrations/notion_dogoo/lib/src/objects/objects.dart
+++ packages/integrations/notion_dogoo/lib/src/objects/objects.dart
@@ -1,4 +1,3 @@
 export 'blocks/block.dart';
-export 'blocks/rich_text/rich_text.dart';
 export 'common/common.dart';
 export 'parents/parent.dart';
