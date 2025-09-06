1e88eed990e173dcd2894b632b59270e0295c528
Kangmin An <hkkm612@gmail.com>
2025-09-06 09:20:31 +0900

    feat: remove `notion_dogoo` from `common` directory (#23)
    feat: remove old package

--- packages/common/notion_dogoo/lib/src/constants/user_type.dart
+++
@@ -1,4 +0,0 @@
-enum UserType {
-  person,
-  bot,
-}
--- packages/common/notion_dogoo/lib/src/objects/objects.dart
+++
@@ -1,5 +0,0 @@
-export 'blocks/block.dart';
-export 'blocks/rich_text/rich_text.dart';
-export 'file.dart';
-export 'parents/parent.dart';
-export 'user.dart';
